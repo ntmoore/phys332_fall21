bd1f0a8cb897d175dee254a798b80cb5fabe9244
emmar2000 <70343971+emmar2000@users.noreply.github.com>
2021-10-13 12:28:33 -0500

    Update 10-13-2bit-adder.v

--- 10-13-2bit-adder.v
+++ 10-13-2bit-adder.v
@@ -4,25 +4,26 @@
 // code is initially broken.  See attached photos for intended design
 
 module example1(
-    input [1:0] swt,
-    output [1:0] led
+    input [3:0] swt,
+    output [2:0] led
     );
-    wire a1, a0, b1, b0;
-    wire s1, s0, cout;
+    wire a1, a0, b1, b0; // inputs
+    wire s1, s0, cout; // outputs
+    wire c_out0 // intermediate
     
     // global inputs
     assign a0 = swt[0];
-    assign a1 = swt[0];
-    assign b0 = swt[0];
-    assign b1 = swt[0];
+    assign a1 = swt[1];
+    assign b0 = swt[2];
+    assign b1 = swt[3];
     
     // global outputs
     assign led[0] = s0;
     assign led[1] = s1;
     assign led[2] = cout;
     
-    fulladder add0(a,b,c_in, c_out, s1);
-    fulladder add1(a,b,c_in, cout, s1);
+    fulladder add0(a0,b0, 0, c_out0, s0);
+    fulladder add1(a1,b1,c_out0, cout, s1);
         
 endmodule 
 
