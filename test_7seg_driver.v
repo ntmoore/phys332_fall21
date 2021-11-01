bacd561f5cf8b44489816affd950bd76ab99b164
Nathan Moore <ntmoore@gmail.com>
2021-10-31 21:18:31 -0500

    Create test_7seg_driver.v

---
+++ test_7seg_driver.v
@@ -0,0 +1,3 @@
+// What does a hardware wrapper for a module you'd like to test look like?  See below
+// for a top-level modeule that serves to send signals to a 7-segment display driver (partically finished).
+//
