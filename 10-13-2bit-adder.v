aa5e8640e557e09b4c15493458d0653ff123b240
Nathan Moore <ntmoore@gmail.com>
2021-10-31 21:56:06 -0500

    rewrite so that a clear test-wrapper is visible.  Should work?

--- 10-13-2bit-adder.v
+++ 10-13-2bit-adder.v
@@ -3,26 +3,33 @@
 // this is a 2bit + 2bit adder for Nexys4 DDR board 
 // code is initially broken.  See attached photos for intended design
 
-module example1(
-    //Need to declare four switches
+module test_two_bit_adder(    
     input [3:0] SW,
-    output [2:0] LED
+    output [2:0] LED);
+    
+    two_bit_adder add_2(SW[3:2], SW[1:0], LED[2:0]);
+    
+endmodule
+        
+module two_bit_adder(
+    input [1:0] a,
+    input [1:0 b,
+    output [2:0] sum
     );
+
     wire a1, a0, b1, b0;
+    assign a[1] = a1;           
+    assign a[0] = a0; 
+    assign b[1] = b1;           
+    assign b[0] = b0; 
+    
     wire s1, s0, cout;
+    assign sum[2] = cout;           
+    assign sum[1] = s1; 
+    assign sum[0] = s0;           
+   
     wire c_mid; // intermedite wire
-    
-    // global inputs
-    assign a0 = SW[0];
-    assign a1 = SW[1];
-    assign b0 = SW[2];
-    assign b1 = SW[3];
-    
-    // global outputs
-    assign LED[0] = s0;
-    assign LED[1] = s1;
-    assign LED[2] = cout;
-    
+        
     fulladder add0(a0, b0, 0, c_mid, s0);
     fulladder add1(a1, b1, c_mid, cout, s1);
         
