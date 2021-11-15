048b8aa752ec6c672c792f5362d64bfc6fae947e
Nathan Moore <ntmoore@gmail.com>
2021-11-15 16:44:08 -0600

    50MHz and 1kHz implementations
    Example implementations for 50MHz (no counter) and 1kHz (17 bit counter) states

--- 11-15_clocks.v
+++ 11-15_clocks.v
@@ -1,6 +1,6 @@
 module implement_clocks(
     input CLK100MHZ,
-    output [6:8] JA,
+    output [7:9] JA,
     output [2:0] LED,
     );
     
@@ -14,7 +14,8 @@ module implement_clocks(
   // sending oth clocks to the PMOD pins on the lower JA header
     assign JA[7] = CLK_50MHZ;
     assign JA[8] = CLK_1kHZ;
-
+    assign JA[9] = 1'b1;
+    
     clk_50MHz_20ns  gate0(CLK100MHZ, CLK_50MHZ);
     clk_1kHz_ims    gate1(CLK100MHZ, CLK_1kHZ);
 
@@ -27,16 +28,35 @@ module clk_50MHz_20ns(
     
     // 100MHZ is 10ns cycles.  
     // I want 50MHz output, 20ns cycles
-    //
-    reg [2:0] ctr=0; //????
     
-    // is this correct?  Doesn't seem quite right
+    // for this very simple counter (clock divider), the output can also be the state bit.
     always @ (posedge incoming_CLK100MHZ) begin
-        if(ctr==1) begin
+        if(outgoing_CLK_50MHz_20ns==1) begin
             outgoing_CLK100HZ <= 1'b0;
-            ctr <= ctr + 1;            
-        end else if(ctr==2) begin
+        end else begin
             outgoing_CLK100HZ <= 1'b1;
+        end         
+    end
+endmodule
+
+module clk_1kHz_ims(
+    input incoming_CLK100MHZ,
+    output reg outgoing_CLK
+    );
+    
+    // 100MHZ is 10ns cycles.  
+    // I want 1kHz output, 1ms cycles
+    // 1ms = 1_000 us = 1_000_000 ns, so # of cycles needed is
+    // 1_000_000 ns / 10 ns = 100k cycles
+    // 2^17 = 131072
+    reg [16:0] ctr=0;
+    
+    always @ (posedge incoming_CLK100MHZ) begin
+        if(ctr==49_999) begin
+            outgoing_CLK <= 1'b0;
+            ctr <= ctr + 1;            
+        end else if(ctr==99_999) begin
+            outgoing_CLK <= 1'b1;
             ctr <= 0;
         end else begin
             ctr <= ctr + 1;
@@ -44,28 +64,6 @@ module clk_50MHz_20ns(
     end
 endmodule
 
-module clk_1kHz_ims(
-    input incoming_CLK100MHZ,
-    output reg outgoing_CLK_1kHz_1ms
-    );
-    
-    // 100MHZ is 10ns cycles.  
-    // I want 1kHz output, 1ms cycles
-    //
-    reg [30:0] ctr=0; // ???
-    
-    // is this correct?  Doesn't seem quite right
-    always @ (posedge incoming_CLK100MHZ) begin
-        if(ctr==49_999) begin
-            outgoing_CLK100HZ <= 1'b0;
-            ctr <= ctr + 1;            
-        end else if(ctr==99_999) begin
-            outgoing_CLK100HZ <= 1'b1;
-            ctr <= 0;
-        end else begin
-            ctr <= ctr + 1;
-        end         
-    end
 
 module clk_10MHz_100ns(
     input incoming_CLK100MHZ,
