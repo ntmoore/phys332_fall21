68110ca4032bdac167e28ebf24360d1d0c878f2b
Emma Rector <70343971+emmar2000@users.noreply.github.com>
2021-11-16 17:55:37 -0600

    Implement 1Hz clock

--- 11-15_clocks.v
+++ 11-15_clocks.v
@@ -182,7 +182,23 @@ module clk_1Hz_1000ms(
     output reg outgoing_CLK
     );
     
-    always @ (posedge incoming_CLK100MHZ) begin
+    // creates 1HZ clock from a 100MHZ clock
+    // 1HZ clock has a period of 1 second = 1000ms
+    // 100MHz is 100,000,000 cycles
+    // log2(10,0000,000) = 26.6, so 27 bits needed for counter
+    reg[26:0] ctr;
     
+    always @ (posedge incoming_CLK100MHZ) begin
+        if (ctr == 49_999_999) begin
+            outgoing_CLK <= 1'b1;
+            ctr <= ctr + 1;            
+        end
+        else if (ctr == 99_999_999) begin
+            outgoing_CLK <= 1'b0;
+            ctr <= 0;
+        end
+        else begin
+            ctr <= ctr + 1;
+        end
     end
 endmodule
